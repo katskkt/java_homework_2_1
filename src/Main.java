9dabc23b42c7d727ab97e29409f12aa0c3616835
Ekaterina <katskktt@gmail.com>
2026-02-11 11:08:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+        int rubles = 20;
+
+        int bonus = ticketPrice / rubles;
+
+        System.out.println("Начислено бонусных миль: " + bonus);
+    }
+}
